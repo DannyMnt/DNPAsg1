52257fb73e3de2a2e0a392176774485c5512ca40
Danny <>
2024-09-08 14:18:43 +0200

    test3

--- InMemoryRepositories/PostInMemoryRepository.cs
+++ InMemoryRepositories/PostInMemoryRepository.cs
@@ -7,7 +7,6 @@ namespace InMemoryRepositories;
 public class PostInMemoryRepository : IPostRepository
 {
     private List<Post?> posts;
-    private Post? post2;
     private Post? GetPostById(int id)
     {
         return posts.SingleOrDefault(p => p.Id == id) ?? throw new InvalidOperationException($"Post with ID '{id}' not found");
